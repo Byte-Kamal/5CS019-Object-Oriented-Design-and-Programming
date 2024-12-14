fb58fb4dff6b8e4f78b37f991d478341748e8533
Byte-Kamal <bytebuilder.kamal@gmail.com>
2024-12-14 12:37:00 +0545

    Folder for week 4

---
+++ Week4/tutorial/Calculator.java
@@ -0,0 +1,18 @@
+package tutorial;
+
+public class Calculator {
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
